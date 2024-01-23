9c465ea96110a4ac5013990f3dcd3d189c7e30cc
root <root@Florian>
2024-01-23 12:37:00 +0100

    test deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Route, Routes } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Routes>
+      <Route
+        path='/'
+        element={
+          <>
+            <App />
+          </>
+        }
+      />
+    </Routes>
   </React.StrictMode>
 );
