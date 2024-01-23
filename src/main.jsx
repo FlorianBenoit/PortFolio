6208627b2f0f0895aebe1316b8e48fab9d4a8456
root <root@Florian>
2024-01-23 12:42:01 +0100

    test deploy

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Routes>
       <Route
-        path='/'
+        path='*'
         element={
           <>
             <App />
