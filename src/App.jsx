8cf76dbd4f1d4dd48860c5c035194c379baa0a33
root <root@Florian>
2024-01-24 11:01:38 +0100

    test deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Error404 from "./components/404/404";
 import Home from "./components/Home/Home";
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
       {" "}
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='*' element={<Home />} />
+        <Route path='*' element={<Error404 />} />
       </Routes>
     </>
   );
---
+++ src/components/404/404.jsx
@@ -0,0 +1,7 @@
+import "./404.css";
+
+function Error404() {
+  return <div className='error'>Error</div>;
+}
+
+export default Error404;
